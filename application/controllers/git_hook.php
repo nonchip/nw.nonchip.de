16e26fde2c3a0df196397b54966e35efda541252
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2013-06-21 21:47:10 +0000

    modified git_hook to only update if commit contains #publish

--- application/controllers/git_hook.php
+++ application/controllers/git_hook.php
@@ -4,6 +4,12 @@ class Git_hook extends CI_Controller {
 
   public function index()
   {
-  	passthru('git pull');
+    if($this->input->post('payload')){
+      $doit=false;
+      foreach(json_decode($this->input->post('payload'))->commits as $c){
+        if(strpos($c->message,'#publish') !== false)
+      }
+      passthru('git pull');
+    }
   }
 }

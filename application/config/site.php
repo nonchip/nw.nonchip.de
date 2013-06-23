204827238fec81cf522fb4a9324ca3b1585b6eae
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2013-06-23 18:27:35 +0000

    style #publish

--- application/config/site.php
+++ application/config/site.php
@@ -6,4 +6,6 @@ $config['site']['heading']='Die Nachtw&ouml;lfe';
 $config['site']['style']=array(
   'header_height'=>'5em',
   'nav_width'=>'10em',
+  'bgcolor'=>'#181000',
+  'content_bgcolor'=>'#FFF3E8',
 );
--- application/views/head.php
+++ application/views/head.php
@@ -4,25 +4,25 @@
     <title><?=$site['title']?></title>
     <style>
       body{
-        background: #000;
+        background: <?=$site['style']['bgcolor']?>;
         font-family: Arial;
       }
       div#heading{
-        color: #FFF3E8;
+        color: <?=$site['style']['content_bgcolor']?>;
         position: fixed;
         height: <?=$site['style']['header_height']?>;
         left: 0px;
         top: 0px;
         right: 0px;
-        /* http://www.colorzilla.com/gradient-editor/#45484d+0,000000+100;Black+3D+%231 */
-        background: #45484d; /* Old browsers */
-        background: -moz-linear-gradient(top, #45484d 0%, #000000 100%); /* FF3.6+ */
-        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#45484d), color-stop(100%,#000000)); /* Chrome,Safari4+ */
-        background: -webkit-linear-gradient(top, #45484d 0%,#000000 100%); /* Chrome10+,Safari5.1+ */
-        background: -o-linear-gradient(top, #45484d 0%,#000000 100%); /* Opera 11.10+ */
-        background: -ms-linear-gradient(top, #45484d 0%,#000000 100%); /* IE10+ */
-        background: linear-gradient(to bottom, #45484d 0%,#000000 100%); /* W3C */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#45484d', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
+        /* http://www.colorzilla.com/gradient-editor/#000000+0,000000+100;Black+3D+%231 */
+        background: <?=$site['style']['bgcolor']?>; /* Old browsers */
+        background: -moz-linear-gradient(top, #000000 0%, <?=$site['style']['bgcolor']?> 70%); /* FF3.6+ */
+        background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#000000), color-stop(70%,<?=$site['style']['bgcolor']?>)); /* Chrome,Safari4+ */
+        background: -webkit-linear-gradient(top, #000000 0%,<?=$site['style']['bgcolor']?> 70%); /* Chrome10+,Safari5.1+ */
+        background: -o-linear-gradient(top, #000000 0%,<?=$site['style']['bgcolor']?> 70%); /* Opera 11.10+ */
+        background: -ms-linear-gradient(top, #000000 0%,<?=$site['style']['bgcolor']?> 70%); /* IE10+ */
+        background: linear-gradient(to bottom, #000000 0%,<?=$site['style']['bgcolor']?> 70%); /* W3C */
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#000000', endColorstr='<?=$site['style']['bgcolor']?>',GradientType=0 ); /* IE6-9 */
       }
       div#heading h1{
         margin: 0.5em;
@@ -36,7 +36,7 @@
         left: 0px;
         top: <?=$site['style']['header_height']?>;
         bottom: 0px;
-        background: #000 url(<?=base_url('static/sidebar-bg-horde.png')?>);
+        background: <?=$site['style']['bgcolor']?> url(<?=base_url('static/sidebar-bg-horde.png')?>);
         background-size: 100%;
         background-repeat: no-repeat;
       }
@@ -47,11 +47,15 @@
         left: <?=$site['style']['nav_width']?>;
         top: <?=$site['style']['header_height']?>;
         bottom: 0px;
-        min-width: 60%;
-        background: #FFF3E8;
-        -webkit-box-shadow: 0.1em 0.1em 1em #FFF3E8;
-           -moz-box-shadow: 0.1em 0.1em 1em #FFF3E8;
-                box-shadow: 0.1em 0.1em 1em #FFF3E8;
+        min-width: 75%;
+        background: <?=$site['style']['content_bgcolor']?>;
+        -webkit-box-shadow: 0.1em 0.1em 1em <?=$site['style']['content_bgcolor']?>;
+           -moz-box-shadow: 0.1em 0.1em 1em <?=$site['style']['content_bgcolor']?>;
+                box-shadow: 0.1em 0.1em 1em <?=$site['style']['content_bgcolor']?>;
+      }
+
+      img#tabard{
+        float: right;
       }
     </style>
   </head>
@@ -66,3 +70,4 @@
     <div id='nav'>
     </div>
     <div id='content'>
+      <img id='tabard' src='<?=base_url('static/tabard.png')?>'>

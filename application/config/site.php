2c1efd2411318ed3dd8656bf6360bd8e995bdf87
Michael Zimmer <me@nonchip.de>
2013-06-24 11:13:55 +0300

    092077699fc72061ea169b9cc780a3fd75d6368e didn't stay compatible... #publish

--- application/config/site.php
+++ application/config/site.php
@@ -1,9 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['site']['title']='NW';
-$config['site']['heading']='Die Nachtw&ouml;lfe';
+$config['title']='NW';
+$config['heading']='Die Nachtw&ouml;lfe';
 
-$config['site']['style']=array(
+$config['style']=array(
   'header_height'=>'5em',
   'nav_width'=>'10em',
   'bgcolor'=>'#181000',

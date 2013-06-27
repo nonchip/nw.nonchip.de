19da317a743ab9aed7cae87c58dc7695ce9c3b29
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2013-06-27 11:13:37 +0000

    fixed merge fuckup #publish

--- application/config/site.php
+++ application/config/site.php
@@ -3,7 +3,7 @@
 $config['title']='NW';
 $config['heading']='Die Nachtw&ouml;lfe';
 
-$config['site']['style']=array(
+$config['style']=array(
   'header_height'=>'6.5em',
   'nav_width'=>'10em',
   'bgcolor'=>'#181000',
